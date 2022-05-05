1d33eb2bfd9592aaff04bf3b4b0bf325f39e88d7
Somik Dmitry <104918094+SomikD@users.noreply.github.com>
2022-05-05 18:41:51 +0300

    Changes
    Изменения в коде
    прописал void

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,18 +1,19 @@
-package out;
+package Calculator;
 
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
-public class Main {
+public class Calculator {
     static Scanner scanner = new Scanner(System.in);
     static int num1, num2;
     static char operation;
     static int result;
 
-    public static char[] main(String[] args) {
+
+    public static void main(String[] args) {
         System.out.println("Введите арифметическое выражение арабскими или римскими числами от I(1) до X(10) ");
         String userInput = scanner.nextLine();
-        char[] under_char = new char[10];
+        char[] under_char = new char[4];
         for (int i = 0; i < userInput.length(); i++) {
             under_char[i] = userInput.charAt(i);
             if (under_char[i] == '+') {
@@ -26,11 +27,8 @@ public class Main {
             }
             if (under_char[i] == '/') {
                 operation = '/';
-            } else {
-                System.out.println("Введен не допустимый знак операции");
             }
-            return under_char;
-        }
+    }
 
 
 
@@ -41,20 +39,20 @@ public class Main {
         String string03 = stable01.trim();
         num1 = romanToNumber(stable00);
         num2 = romanToNumber(string03);
-        if (num1 < 0 && num2 < 0) {
+        if (num1 < 11 && num2 <11) {
             result = 0;
         } else {
             result = calculated(num1, num2, operation);
-            System.out.println("Результат для римскиз цифр");
+            System.out.println("Результат для римских цифр");
             String resultRoman = convertNumRoman(result);
             System.out.println(stable00 + " " + operation + " " + string03 + " = " + resultRoman);
         }
+
         num1 = Integer.parseInt(stable00);
         num2 = Integer.parseInt(string03);
         result = calculated(num1, num2, operation);
         System.out.println("  Результат для арабскихз чисел  ");
         System.out.println(num1 + " " + operation + " " + num2 + " = " + result);
-        return under_char;
     }
 
 
@@ -62,8 +60,7 @@ public class Main {
     private static String convertNumRoman (int numArabian) {
         String[] roman = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"
         };
-        final String s = roman[numArabian];
-        return s;
+        return roman[numArabian];
     }
 
     private static int romanToNumber (String roman) {
@@ -87,13 +84,13 @@ public class Main {
             }else if(roman.equals("IX")) {
                 return 9;
             }else if(roman.equals("X")) {
-                    return 10;
-                }
-            } catch (InputMismatchException e) {
-                throw new InputMismatchException("Неверный формат данных");
+                return 10;
             }
-            return  -1;
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException("Нет таких данных");
         }
+        return -1;
+    }
 
 
     public static int calculated(int number1, int number2, char op) {
@@ -123,4 +120,6 @@ public class Main {
         }
         return result;
     }
+
 }
+
